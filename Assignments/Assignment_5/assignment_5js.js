63db714a945f7f00449fc8611bba673bc74b1342
maslychm <maslychm@gmail.com>
2019-10-24 18:49:53 -0400

    match the requirement

--- Assignments/Assignment_5/assignment_5js.js
+++ Assignments/Assignment_5/assignment_5js.js
@@ -22,7 +22,7 @@ function render() {
 }
 
 function initRenderer() {
-    renderer = new THREE.WebGLRenderer({antialias : true});
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(WIDTH, HEIGHT);
     renderer.setClearColor(0x888888, 1);
     document.body.appendChild(renderer.domElement);
@@ -30,31 +30,38 @@ function initRenderer() {
 
 function initScene() {
     scene = new THREE.Scene();
+
+    //Load background texture
+    let loader = new THREE.TextureLoader();
+    loader.load("grass.jpg",
+        function (texture) {
+            scene.background = texture;
+        });
 }
 
 function initCamera() {
     camera = new THREE.PerspectiveCamera(50, WIDTH / HEIGHT, 1, 50);
-    camera.position.set(7,7,7);
-    camera.lookAt(0,0,0);
-    cameraControls = new THREE.OrbitControls (camera, renderer.domElement);
+    camera.position.set(4, 4, 4);
+    camera.lookAt(0, 0, 0);
+    cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
 }
 
 function eventHandlers() {
-    cameraControls.addEventListener("change", 
+    cameraControls.addEventListener("change",
         function () {
             camera.updateProjectionMatrix();
             render();
         });
 
     window.addEventListener("resize",
-    function() {
-        WIDTH = window.innerWidth;
-        HEIGHT = window.innerHeight;
-        renderer.setSize(WIDTH, HEIGHT);
-        camera.aspect = WIDTH / HEIGHT;
-        camera.updateProjectionMatrix();
-        render();
-    });
+        function () {
+            WIDTH = window.innerWidth;
+            HEIGHT = window.innerHeight;
+            renderer.setSize(WIDTH, HEIGHT);
+            camera.aspect = WIDTH / HEIGHT;
+            camera.updateProjectionMatrix();
+            render();
+        });
 }
 
 function vertexShader() {
@@ -65,10 +72,11 @@ function vertexShader() {
         varying vec2 vUV;
 
         void main() {
+
+            // Calculate points in of model in world space
             vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
 
-            // Temporary Solution
-            // globalNormal = vec3(modelMatrix * vec4(normal, 1.0));
+            // Get points for the texture
             vUV = uv;
 
             // Apply Inverse Transpose Model Matrix (Model in Global space) to normal 
@@ -84,24 +92,23 @@ function fragmentShader() {
         uniform vec3 _skyLight;
         uniform vec3 _groundLight;
         uniform vec3 _up;
-        uniform vec3 _materialColor;
-        uniform sampler2D _brick;
+        uniform sampler2D _concrete;
 
         varying vec3 globalNormal;
         varying vec2 vUV;
 
         void main() {
-            vec3 brickColor;
-
             // Weight the amount of light from each side
             float w = 0.5 * (1.0 + dot(normalize(globalNormal), normalize(_up)));
 
             // Apply the light colors and combine with material
             vec3 uIncidentLight = w * _skyLight + (1.0 - w) * _groundLight;
-            vec3 uReflectedColor = uIncidentLight * _materialColor;
-
-            // brickColor = texture2D(_brick,vUV).rgb;
-            //uReflectedColor = uIncidentLight * brickColor;
+            
+            // Get the color of texture at a point
+            vec3 concreteColor = texture2D(_concrete,vUV).rgb;
+            
+            // Combine color at pixel + light to find shading
+            vec3 uReflectedColor = uIncidentLight * concreteColor;
 
             gl_FragColor = vec4(uReflectedColor, 1.0);
         }
@@ -115,17 +122,16 @@ function applyShaders() {
 
     // Get ModelMatrix to inverse, transpose and apply to normal and shader
     invTModelMatrix = new THREE.Matrix4();
-    invTModelMatrix.getInverse(mesh2.matrixWorld , false);
+    invTModelMatrix.getInverse(mesh2.matrixWorld, false);
 
     let uniforms = {
-        _up : {type : 'vec3', value : new THREE.Vector3(0,1,0)},
-        _skyLight : {type : 'vec3', value : new THREE.Color(1,1,1)},
-        _groundLight : {type : 'vec3', value : new THREE.Color(0,0,0)},
-        _materialColor : {type : 'vec3', value : new THREE.Color(1, 0, 0)},
-        _invTModelMatrix : {type : 'mat4', value : invTModelMatrix.transpose()}, // Transpose 
-        _brick: {
-            type: "t",
-            value: new THREE.TextureLoader().load ("brick.jpg",function(texture){render();}) 
+        _up: { type: 'vec3', value: new THREE.Vector3(0, 1, 0) },
+        _skyLight: { type: 'vec3', value: new THREE.Color(1, 1, 1) },
+        _groundLight: { type: 'vec3', value: new THREE.Vector3(0, 1, 0) },
+        _invTModelMatrix: { type: 'mat4', value: invTModelMatrix.transpose() }, // Transpose
+        _concrete: {
+            type: 't',
+            value: new THREE.TextureLoader().load("concrete.jpeg", function (texture) { render(); })
         }
     }
 
